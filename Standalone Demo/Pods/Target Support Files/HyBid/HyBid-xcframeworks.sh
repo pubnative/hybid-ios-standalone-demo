1bdfb31f92cf49a9299e58db521238f3056a5a35
Can SOYKARAFAKILI <can.soykarafakili@gmail.com>
2022-03-24 15:13:42 +0100

    pod HyBid v:2.12.1 is added

--- Standalone Demo/Pods/Target Support Files/HyBid/HyBid-xcframeworks.sh
+++ Standalone Demo/Pods/Target Support Files/HyBid/HyBid-xcframeworks.sh
@@ -105,5 +105,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/HyBid/PubnativeLite/PubnativeLite/OMSDK-1.3.26/OMSDK_Pubnativenet.xcframework" "HyBid/Core" "framework" "ios-arm64_i386_x86_64-simulator" "ios-arm64_armv7"
+install_xcframework "${PODS_ROOT}/HyBid/PubnativeLite/PubnativeLite/OMSDK-1.3.29/OMSDK_Pubnativenet.xcframework" "HyBid/Core" "framework" "ios-arm64_armv7" "ios-arm64_i386_x86_64-simulator"
 
