abc25ef1527a1015f21d986bd1800bcbb6594339
Can SOYKARAFAKILI <can.soykarafakili@gmail.com>
2023-02-14 18:43:13 +0300

    pod HyBid 2.17.1-beta is added

--- Objective-C/Standalone Demo - ObjC/Pods/Target Support Files/HyBid/HyBid-xcframeworks.sh
+++ Objective-C/Standalone Demo - ObjC/Pods/Target Support Files/HyBid/HyBid-xcframeworks.sh
@@ -17,11 +17,17 @@ RSYNC_PROTECT_TMP_FILES=(--filter "P .*.??????")
 variant_for_slice()
 {
   case "$1" in
-  "OMSDK_Pubnativenet.xcframework/ios-arm64_i386_x86_64-simulator")
-    echo "simulator"
-    ;;
   "OMSDK_Pubnativenet.xcframework/ios-arm64_armv7")
     echo ""
+    ;;
+  "OMSDK_Pubnativenet.xcframework/tvos-arm64_x86_64-simulator")
+    echo "simulator"
+    ;;
+  "OMSDK_Pubnativenet.xcframework/tvos-arm64")
+    echo ""
+    ;;
+  "OMSDK_Pubnativenet.xcframework/ios-arm64_i386_x86_64-simulator")
+    echo "simulator"
     ;;
   esac
 }
@@ -29,11 +35,17 @@ variant_for_slice()
 archs_for_slice()
 {
   case "$1" in
-  "OMSDK_Pubnativenet.xcframework/ios-arm64_i386_x86_64-simulator")
-    echo "arm64 i386 x86_64"
-    ;;
   "OMSDK_Pubnativenet.xcframework/ios-arm64_armv7")
     echo "arm64 armv7"
+    ;;
+  "OMSDK_Pubnativenet.xcframework/tvos-arm64_x86_64-simulator")
+    echo "arm64 x86_64"
+    ;;
+  "OMSDK_Pubnativenet.xcframework/tvos-arm64")
+    echo "arm64"
+    ;;
+  "OMSDK_Pubnativenet.xcframework/ios-arm64_i386_x86_64-simulator")
+    echo "arm64 i386 x86_64"
     ;;
   esac
 }
@@ -117,5 +129,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/HyBid/PubnativeLite/PubnativeLite/OMSDK-1.3.29/OMSDK_Pubnativenet.xcframework" "HyBid/Core" "framework" "ios-arm64_i386_x86_64-simulator" "ios-arm64_armv7"
+install_xcframework "${PODS_ROOT}/HyBid/PubnativeLite/PubnativeLite/OMSDK-1.4.2/OMSDK_Pubnativenet.xcframework" "HyBid/Core" "framework" "ios-arm64_armv7" "ios-arm64_i386_x86_64-simulator"
 
