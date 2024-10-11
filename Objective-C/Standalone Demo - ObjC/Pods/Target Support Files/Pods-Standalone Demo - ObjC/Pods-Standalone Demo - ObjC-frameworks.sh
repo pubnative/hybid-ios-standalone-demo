355ce71f45ea2d5125eb56de361bf303a45b4298
Can SOYKARAFAKILI <can.soykarafakili@gmail.com>
2024-10-11 15:25:56 +0200

    pod HyBid v:3.1.1 is added

--- Objective-C/Standalone Demo - ObjC/Pods/Target Support Files/Pods-Standalone Demo - ObjC/Pods-Standalone Demo - ObjC-frameworks.sh
+++ Objective-C/Standalone Demo - ObjC/Pods/Target Support Files/Pods-Standalone Demo - ObjC/Pods-Standalone Demo - ObjC-frameworks.sh
@@ -177,10 +177,12 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/HyBid/HyBid.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/ATOM-Standalone/ATOM.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/HyBid/Core/OMSDK_Pubnativenet.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/HyBid/HyBid.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/ATOM-Standalone/ATOM.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/HyBid/Core/OMSDK_Pubnativenet.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
